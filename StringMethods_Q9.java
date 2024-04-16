0fd8b5bb5bfdfe255602df4ebf906a1825b81ade
Sudipta987 <rajakar21162@gmail.com>
2024-04-16 20:38:39 +0530

    Q9 : W.A.P to implement some most important methods of String.

---
+++ StringMethods_Q9.java
@@ -0,0 +1,30 @@
+//Q9 : W.A.P to implement some most important methods of String.
+package Java.Array.String;
+import java.util.Scanner;
+public class StringMethods_Q9 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        //System.out.println("Enter a String  as input: ");
+        //String str=sc.nextLine();
+        System.out.println("Enter your name : ");
+        String name=sc.nextLine();
+        //1 name=name.toLowerCase();
+        //2 name=name.toUpperCase();
+        //3 name=name.trim();
+        //4   name=name.replace('a','o');
+        //5  str=str.replace("My","Your");
+        //6  System.out.println(str.startsWith("My")); 
+        //7   System.out.println(str.endsWith("kar"));
+        //8   System.out.println(name.indexOf('d'));
+        //9 System.out.println(name.indexOf("dip"));
+        //10    System.out.println(name.indexOf('a',7));
+        //11     System.out.println(name.lastIndexOf('a'));
+        //12    System.out.println(name.lastIndexOf('a',7));
+        //13     System.out.println(name.equals("Sudipta kar"));
+         //14   System.out.println(name.equalsIgnoreCase("sudipta kar"));
+        //15     System.out.println(name.concat("kar"));
+      // System.out.println("Now the String is : "+str);
+        System.out.println("Your name is : "+name);
+        sc.close();
+    }
+}
